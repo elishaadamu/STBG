4dcd31ed168365cd47c751dcad40889b1bc73b98
elishaadamu <elishadamu97@gmail.com>
2026-02-02 21:22:18 +0100

    correct set pin modal in set pin page

--- app.py
+++ app.py
@@ -1083,4 +1083,4 @@ async def analyze_projects(
 # =============================================================================
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
